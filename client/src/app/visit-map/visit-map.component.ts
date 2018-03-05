01792ca9d1950a86e3d9352846fd9302d18355cc
Ana Recuero <anarecsa@gmail.com>
2018-03-05 21:13:02 +0100

    view first day visits

--- client/src/app/visit-map/visit-map.component.ts
+++ client/src/app/visit-map/visit-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PlannerService } from '../../services/planner.service';
+import { durationColors } from "../../interfaces/duration-colors";
 
 @Component({
   selector: 'app-visit-map',
@@ -11,6 +12,8 @@ export class VisitMapComponent implements OnInit {
   plan;
   tripID;
   cityID;
+  visits;
+  dayPos:number = 0;
   title: string = 'My first AGM project';
   lat: number = 51.678418;
   lng: number = 7.809007;
@@ -22,6 +25,7 @@ export class VisitMapComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getVisits();
     this.route.params.subscribe( params => {
       this.tripID = params["id"];
       this.getPlan(this.tripID)
@@ -30,8 +34,32 @@ export class VisitMapComponent implements OnInit {
   getPlan(id){
     this.planner.getPlan(id).subscribe( res => {
       this.plan = res;
+      console.log(this.plan)
       this.cityID = res.city.id;
     })
   }
-
+  getVisits(){
+    this.route.params.subscribe( params => {
+      this.planner.getVisits(params["id"], this.dayPos).subscribe( res => {
+        console.log(res)
+        this.visits = res
+      })
+    })
+  }
+  getHeight(sec){
+    return ((sec/ 3600) * 40) + "px"
+  }
+  getBackground(sec){
+    if(sec < 7200 ){
+      return durationColors["7200"]
+    } else if(sec < 10800 ){
+      return durationColors["10800"]
+    } else if(sec < 14400 ){
+      return durationColors["14400"]
+    } else if(sec < 18000 ){
+      return durationColors["18000"]
+    } else{
+      return durationColors["21600"]
+    }
+  }
 }
