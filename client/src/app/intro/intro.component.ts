bc0e72014d174430a74ee3fee30c43efc686f23b
Ana Recuero <anarecsa@gmail.com>
2018-03-08 16:19:00 +0100

    styled intro

--- client/src/app/intro/intro.component.ts
+++ client/src/app/intro/intro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router:Router, 
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
   }
